a7a78079bd529d09ed6369c7aba44833c5c18cee
the-last-airblender <40359106+the-last-airblender@users.noreply.github.com>
2018-06-18 15:52:29 +0200

    Update png_to_matrix.py

--- Research practicum/png_to_matrix.py
+++ Research practicum/png_to_matrix.py
@@ -3,9 +3,14 @@ from PIL import Image
 import numpy as np
 import math as m
 
+# Dit zet de foto om naar een grayscale matrix
+
 im = imageio.imread("0.1.jpg" )
 
 #print im.shape
+
+
+# Hier probeer ik een plaatje van een normale verdeling te genereren om de normale verdeling fit functie te kunnen testen. 
 
 def Norm(x,y,S_x,S_y,M_x,M_y):
     return 255 * m.exp( - ((x - M_x) ** 2)/(S_x) - ((y - M_y) ** 2)/(S_y))
@@ -29,7 +34,10 @@ for i in range(256):
 
 #print max(normaldist)
 
+#fromarray heeft een nparray nodig
 normarray  = np.asarray(normaldist)
+
+# Het gaat tot hier goed, maar fromarray leest het niet helemaal goed
 
 foto = Image.fromarray(normarray,'L')
 foto.show()
